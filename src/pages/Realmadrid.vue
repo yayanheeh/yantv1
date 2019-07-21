2709e07858c260c05768868d715aed556c3af41e
yayanheeh <tokoxp58@gmail.com>
2019-07-21 20:06:01 +0700

    Create Realmadrid.vue

---
+++ src/pages/Realmadrid.vue
@@ -0,0 +1,17 @@
+<template>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://yantv.netlify.com/play/realmadrid' style='border:0px #ffffff none;' name='myiFrame' scrolling='no' frameborder='1' marginheight='0px' marginwidth='0px' height='400px' width='600px' allowfullscreen></iframe></div>
+
+  </template>
+<script>
+  import Yayan from 'src/components/Yayan/Yayan.vue'
+
+  export default {
+    components: {
+      Yayan
+    }
+  }
+
+
+<style>
+
+</style>
