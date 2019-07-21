65451941a2d785aec01d9ea7f7f6471922f76a78
yayanheeh <tokoxp58@gmail.com>
2019-07-21 18:23:51 +0700

    Update ContentFooter.vue

--- src/layout/ContentFooter.vue
+++ src/layout/ContentFooter.vue
@@ -11,8 +11,8 @@
       <div class="copyright text-center">
         &copy; Coded with
         <i class="fa fa-heart heart"></i> by
-        <a href="https://binarcode.com" target="_blank">BinarCode</a>.
-        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
+        <a href="https://yayanheeh.online" target="_blank">Yplay Tv</a>.
+        Designed by <a href="https://www.yayanheeh.online" target="_blank">Yayan He'eh</a>.
       </div>
     </div>
   </footer>
