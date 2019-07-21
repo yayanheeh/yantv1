b12035a44b9283e87aab0785aecced11ba27a4f1
yayanheeh <tokoxp58@gmail.com>
2019-07-21 22:25:43 +0700

    Update Realmadrid.vue

--- src/pages/Realmadrid.vue
+++ src/pages/Realmadrid.vue
@@ -2,113 +2,27 @@
 <div class="video-container"><iframe width="560" height="315" src="https://yantv.netlify.com/play/realmadrid" frameborder="0" allowfullscreen></iframe></div>
 
 
-Meski komposisi pemain kalah dari Juventus, Tottenham mampu memberi tekanan di awal-awal. Heung-Min Son sudah mampu menganjam lewat tembakan dari jarak dekat yang masih menyamping di sisi kanan gawang Gianluigi Buffon.
 
-Juventus gantian mengancam di menit ketujuh. Emre Can menanduk bola dari dalam kotak penalti usai memanfaatkan umpan silang Federico Bernardeschi, namun bola masih melambung tinggi di atas gawang Tottenham.
-
-Tottenham kembali melepas tembakan di menit ke-14. Tembakan Georgiou dari luar kotak penalti belum mengarah ke gawang Juve.
-
-Juventus Vs Tottenham: Gol Spektakuler Kane di Injury Time Menangkan SpursFoto: Thananuwat Srirasant/Getty Images
-
-Sampai laga berjalan setengah jam, Juventus belum terlihat tampil dengan apa yang menjadi ciri khas Maurizio Sarri, yakni penguasaan bolanya.
-
-Si Nyonya Tua malah harus kebobolan di menit ke-31. Erik Lamela berhasil menyambar bola rebound tembakan Parrot di mulut gawang Juve.
-
-Juventus mengancam di menit ke-38. Blaise Matuidi melepas umpan silang ke Ronaldo, namun pria asal Portugal itu meleset dalam menyundul bola.
-
-Tak ada gol yang mampu dicetak Juve hingga turun minum. Babak pertama pun tuntas dengan skor 1-0 untuk Tottenham.
-
-Juventus melakukan empat perubahan di awal babak kedua, salah satunya dengan menarik Mario Mandzukic untuk memainkan Gonzalo Higuain.
-
-Pergantian di lini depan berbuah hasil. Higuain melesakkan gol di menit ke-56 usai memanfaatkan umpan Bernardeschi.
-
-Empat menit berselang Ronaldo membawa Juve berbalik unggul 2-1. Umpan tarik De Sciglio berhasil dituntaskan dengan tembakan kaki kanan.
-
-Tottenham berhasil menyamakan skor menjadi 2-2 di menit ke-65. Lucas Moura lepas dari pengawalan pemain bertahan Juve dan sukses melesakkan gol dari jarak dekat.
-
-Harry Kane mengejutkan seisi stadion di injury time. Dia melepaskan tembakan dari tengah lapangan. Bola langsung meluncur ke gawang Juve tanpa bisa dijangkau Wojciech Szczesny. Gol ini menjadi penutup laga dan Tottenham menang dengan skor 3-2.
-
-Nooruddean
-✔
-@BeardedGenius
- Harry Kane fortunate there. Aimless clearance nestles into the back of the net 
-
- Embedded video
-1,099
-8:34 PM - Jul 21, 2019
-Twitter Ads info and privacy
-366 people are talking about this
-
-Susunan Pemain:
-
-Juventus: Gianluigi Buffon (Wojciech Szczesny 46'); Mattia De Sciglio, Daniel Rugani (Merih Demiral 46'), Leonardo Bonucci (Matthijs de Ligt 63'), Joao Cancelo; Blaise Matuidi (Grigoris Kastanos 76'), Miralem Pjanic (Simone Muratore 76'), Emre Can (Adrien Rabiot 46') ; Cristiano Ronaldo (Matheus Pereira 63'), Mario Mandzukic (Gonzalo Higuain 46'), Federico Bernardeschi.
-
-Tottenham Hotspur: Paulo Gazzaniga; Anthony Georgiou (Harvey White 71'), Japhet Tanganga (Jan Vertonghen 46'), Toby Alderweireld (Juan Foyth 46'), Kyle Walker-Peters (George Marsh 71'); Harry Winks (Jack Roles 63'), Oliver Skipp (Jamie Bowden 90+2'); Erik Lamela (Tanguy Ndombele 63'), Dele Alli (Moussa Sissoko 46'), Son Heung-min (Lucas Moura 46'), Troy Parrott (Harry Kane 46').
-
-
-
-
-Simak Video "Juventini Indonesia Ramaikan Laga Juventus vs Tottenham"
-
-
-(ran/fem)
-juventus tottenham hotspur international champions cup 2019
-Bantu Kami Mengembangkan detikcom, Jawab dan Dapatkan Hadiah Menarik! Klik di Sini
-
-Meski komposisi pemain kalah dari Juventus, Tottenham mampu memberi tekanan di awal-awal. Heung-Min Son sudah mampu menganjam lewat tembakan dari jarak dekat yang masih menyamping di sisi kanan gawang Gianluigi Buffon.
-
-Juventus gantian mengancam di menit ketujuh. Emre Can menanduk bola dari dalam kotak penalti usai memanfaatkan umpan silang Federico Bernardeschi, namun bola masih melambung tinggi di atas gawang Tottenham.
-
-Tottenham kembali melepas tembakan di menit ke-14. Tembakan Georgiou dari luar kotak penalti belum mengarah ke gawang Juve.
-
-Juventus Vs Tottenham: Gol Spektakuler Kane di Injury Time Menangkan SpursFoto: Thananuwat Srirasant/Getty Images
-
-Sampai laga berjalan setengah jam, Juventus belum terlihat tampil dengan apa yang menjadi ciri khas Maurizio Sarri, yakni penguasaan bolanya.
-
-Si Nyonya Tua malah harus kebobolan di menit ke-31. Erik Lamela berhasil menyambar bola rebound tembakan Parrot di mulut gawang Juve.
-
-Juventus mengancam di menit ke-38. Blaise Matuidi melepas umpan silang ke Ronaldo, namun pria asal Portugal itu meleset dalam menyundul bola.
-
-Tak ada gol yang mampu dicetak Juve hingga turun minum. Babak pertama pun tuntas dengan skor 1-0 untuk Tottenham.
-
-Juventus melakukan empat perubahan di awal babak kedua, salah satunya dengan menarik Mario Mandzukic untuk memainkan Gonzalo Higuain.
-
-Pergantian di lini depan berbuah hasil. Higuain melesakkan gol di menit ke-56 usai memanfaatkan umpan Bernardeschi.
-
-Empat menit berselang Ronaldo membawa Juve berbalik unggul 2-1. Umpan tarik De Sciglio berhasil dituntaskan dengan tembakan kaki kanan.
-
-Tottenham berhasil menyamakan skor menjadi 2-2 di menit ke-65. Lucas Moura lepas dari pengawalan pemain bertahan Juve dan sukses melesakkan gol dari jarak dekat.
-
-Harry Kane mengejutkan seisi stadion di injury time. Dia melepaskan tembakan dari tengah lapangan. Bola langsung meluncur ke gawang Juve tanpa bisa dijangkau Wojciech Szczesny. Gol ini menjadi penutup laga dan Tottenham menang dengan skor 3-2.
-
-Nooruddean
-✔
-@BeardedGenius
- Harry Kane fortunate there. Aimless clearance nestles into the back of the net 
-
- Embedded video
-1,099
-8:34 PM - Jul 21, 2019
-Twitter Ads info and privacy
-366 people are talking about this
-
-Susunan Pemain:
-
-Juventus: Gianluigi Buffon (Wojciech Szczesny 46'); Mattia De Sciglio, Daniel Rugani (Merih Demiral 46'), Leonardo Bonucci (Matthijs de Ligt 63'), Joao Cancelo; Blaise Matuidi (Grigoris Kastanos 76'), Miralem Pjanic (Simone Muratore 76'), Emre Can (Adrien Rabiot 46') ; Cristiano Ronaldo (Matheus Pereira 63'), Mario Mandzukic (Gonzalo Higuain 46'), Federico Bernardeschi.
-
-Tottenham Hotspur: Paulo Gazzaniga; Anthony Georgiou (Harvey White 71'), Japhet Tanganga (Jan Vertonghen 46'), Toby Alderweireld (Juan Foyth 46'), Kyle Walker-Peters (George Marsh 71'); Harry Winks (Jack Roles 63'), Oliver Skipp (Jamie Bowden 90+2'); Erik Lamela (Tanguy Ndombele 63'), Dele Alli (Moussa Sissoko 46'), Son Heung-min (Lucas Moura 46'), Troy Parrott (Harry Kane 46').
-
-
-
-
-Simak Video "Juventini Indonesia Ramaikan Laga Juventus vs Tottenham"
-
-
-(ran/fem)
-juventus tottenham hotspur international champions cup 2019
-Bantu Kami Mengembangkan detikcom, Jawab dan Dapatkan Hadiah Menarik! Klik di Sini
-
-
+ <footer class="footer-demo">
+      <div class="container">
+        <nav class="pull-left">
+          <ul>
+            <li>
+              <router-link :to="{path:'/'}">Home</router-link>
+            </li>
+            <li>
+              <router-link :to="{path:'/register'}">Register</router-link>
+            </li>
+          </ul>
+        </nav>
+        <div class="copyright text-center">
+          &copy; Coded with
+          <i class="fa fa-heart heart"></i> by
+          <a href="https://binarcode.com" target="_blank">BinarCode</a>.
+          Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
+        </div>
+      </div>
+    </footer>
 
 
 </template>
