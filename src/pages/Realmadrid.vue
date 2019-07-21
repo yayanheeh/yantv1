84cbacff69d60af283b162a085c6adde5da18f0f
yayanheeh <tokoxp58@gmail.com>
2019-07-21 20:44:05 +0700

    Update Realmadrid.vue

--- src/pages/Realmadrid.vue
+++ src/pages/Realmadrid.vue
@@ -1,8 +1,23 @@
 <template>
-
-<div class="embed-responsive embed-responsive-16by9">
-<iframe src='https://yantv.netlify.com/play/realmadrid' style='border:0px #ffffff none;' name='myiFrame' scrolling='no' frameborder='1' marginheight='0px' marginwidth='0px' height='400px' width='600px' allowfullscreen></iframe></div>
+<div class="video-container"><iframe width="560" height="315" src="https://yantv.netlify.com/play/realmadrid" frameborder="0" allowfullscreen></iframe></div>
   </template>
+<style>
+.video-container {
+position: relative;
+padding-bottom: 56.25%;
+padding-top: 30px; height: 0; overflow: hidden;
+}
+.video-container iframe,
+.video-container object,
+.video-container embed {
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+}
+</style>
+
 <script>
   import Yayan from 'src/components/Yayan/Yayan.vue'
 
