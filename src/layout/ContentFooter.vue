05781efb04f881dbf9da954030c24328087e4050
yayanheeh <tokoxp58@gmail.com>
2019-07-22 19:43:06 +0700

    Update ContentFooter.vue

--- src/layout/ContentFooter.vue
+++ src/layout/ContentFooter.vue
@@ -1,26 +1,7 @@
 <template>
-<script id="dsq-count-scr" src="//yplay-tv.disqus.com/count.js" async></script>
-  <footer class="footer">
-  <div id="disqus_thread"></div>
-<script>
 
-/**
-*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-/*
-var disqus_config = function () {
-this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-*/
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = 'https://yplay-tv.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
-</script>
-    <div class="container-fluid">
+  <footer class="footer">
+      <div class="container-fluid">
       <nav>
         <ul class="footer-menu">
           <li>
