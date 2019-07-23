15ff8d441797e8b92d2e140acca3cd1a6af9faa6
yayanheeh <tokoxp58@gmail.com>
2019-07-23 11:08:13 +0700

    Create FooterMenu.vue

---
+++ src/layout/FooterMenu.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <footer class="footer">
+      <div class="container-fluid">
+      <nav>
+        <ul class="footer-menu">
+          <li>
+            <router-link :to="{path:'/admin'}">yanDashboard yan</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        &copy; Coded with
+        <i class="fa fa-heart heart"></i> by
+        <a href="https://yayanheeh.online" target="_blank">Yplay Tv footer</a>.
+        Designed by <a href="https://www.yayanheeh.online" target="_blank">Yayan He'eh</a>.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+  export default {}
+</script>
+<style>
+</style>
