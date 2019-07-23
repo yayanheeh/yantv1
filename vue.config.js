c9c9476c721f495a2cecbcc56cc77b2f3e0e864f
yayanheeh <tokoxp58@gmail.com>
2019-07-23 22:57:20 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     ]
   },
   pwa: {
-    name: 'Vue Light Bootstrap Dashboard',
+    name: 'Yplay Tv',
     themeColor: '#344675',
     msTileColor: '#344675',
     appleMobileWebAppCapable: 'yes',
