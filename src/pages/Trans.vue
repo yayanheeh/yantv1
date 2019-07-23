fe19ff00093db9d08e326f6d8b000b548ad94cd6
yayanheeh <tokoxp58@gmail.com>
2019-07-23 22:45:53 +0700

    Update Trans.vue

--- src/pages/Trans.vue
+++ src/pages/Trans.vue
@@ -2,7 +2,7 @@
  <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
-            <iframe width="560" height="315" src="http://202.80.222.178/000001/2/ch15010918464887721048/index.m3u8?virtualDomain=000001.live_hls.zte.com" frameborder="0" allowfullscreen></iframe>    </div> <div class="list-group-item">Trans Tv Live 
+            <iframe width="560" height="315" src="https://yantv.netlify.com/ply/trans" frameborder="0" allowfullscreen></iframe>    </div> <div class="list-group-item">Trans Tv Live 
 
 </div></div>
 
