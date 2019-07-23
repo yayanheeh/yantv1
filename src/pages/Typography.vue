b8a74b72fd2be7f9d4dca447bfaab5da2f57339e
yayanheeh <tokoxp58@gmail.com>
2019-07-23 12:56:46 +0700

    Update Typography.vue

--- src/pages/Typography.vue
+++ src/pages/Typography.vue
@@ -3,7 +3,19 @@
 
 <div class="embed-responsive embed-responsive-16by9">
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/T6b5TAey728" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-       </div>   tessst</div>
+       </div>   <div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
+  <div class="toast-header">
+    <img src="..." class="rounded mr-2" alt="...">
+    <strong class="mr-auto">Bootstrap</strong>
+    <small>11 mins ago</small>
+    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body">
+    Hello, world! This is a toast message.
+  </div>
+</div></div>
 
   </template>
 
