4f14153d41aea877b7e9f1a4faa83a4de7eb1dd0
yayanheeh <tokoxp58@gmail.com>
2019-07-23 22:40:23 +0700

    Create Trans.vue

---
+++ src/pages/Trans.vue
@@ -0,0 +1,26 @@
+<template>
+ <div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+            <iframe width="560" height="315" src="http://202.80.222.178/000001/2/ch15010918464887721048/index.m3u8?virtualDomain=000001.live_hls.zte.com" frameborder="0" allowfullscreen></iframe>    </div> <div class="list-group-item">Trans Tv Live 
+
+</div></div>
+
+  </template>
+
+
+
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+<style>
+
+</style>
+
