04bf1700aaa3792488ff249df013fa746c207801
yayanheeh <tokoxp58@gmail.com>
2019-07-23 19:02:37 +0700

    Update FooterMenu.vue

--- src/layout/FooterMenu.vue
+++ src/layout/FooterMenu.vue
@@ -6,7 +6,7 @@
          <img src="https://yayanheeh.github.io/live/logo/rcti.jpg" class="img-thumbnail" alt="Responsive image">
 
         <div class="card-img-overlay text-white d-flex flex-column justify-content-center">
-          
+          <span class="time">Live</span>
          <center><router-link :to="{path:'/admin'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center>
      </div>   
 </div>
@@ -16,7 +16,7 @@
           <img src="https://yayanheeh.github.io/live/logo/rcti.jpg" class="img-thumbnail" alt="Responsive image">
 
         <div class="card-img-overlay text-white d-flex flex-column justify-content-center">
-          
+          <span class="time">Live</span>
          <center><router-link :to="{path:'/admin'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center> </div>
         </div>
         
@@ -29,6 +29,16 @@
     
     
 </template>
+<style>
+.time {
+  position: absolute; z-index: 2;
+  right: 3px; bottom: 3px;
+  padding: 2px 5px;
+  background-color: rgba(0, 0, 0, 0.6);
+  color: white;
+}
+</style>
+
 
 <script>
   export default {}
