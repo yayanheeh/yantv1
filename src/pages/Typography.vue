bddb4a3d33f45768a59990b099bff539063eee86
yayanheeh <tokoxp58@gmail.com>
2019-07-23 12:25:31 +0700

    Update Typography.vue

--- src/pages/Typography.vue
+++ src/pages/Typography.vue
@@ -4,22 +4,21 @@
 <div class="embed-responsive embed-responsive-16by9">
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/T6b5TAey728" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
        </div>   </div>
-
-  <div class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12">
-          <card>
-            <template slot="header">
-              <h4 class="card-title">Light Bootstrap Table Heading</h4>
-              <p class="card-category">Created using Roboto Font Family</p>
-            </template>
-            
-            <div class="embed-responsive embed-responsive-16by9">
-            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/T6b5TAey728" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-          <div class="d-none d-lg-block">tesssttt</div>
-</template>
+<div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
+  <div class="toast-header">
+    <img src="..." class="rounded mr-2" alt="...">
+    <strong class="mr-auto">Bootstrap</strong>
+    <small>11 mins ago</small>
+    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body">
+    Hello, world! This is a toast message.
+  </div>
+</div>
+<br/>
+  </template>
 <script>
   import Yayan from 'src/components/Yayan/Yayan.vue'
 
