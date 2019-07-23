4525e6450eea687c097dbbf8637537507343da22
yayanheeh <tokoxp58@gmail.com>
2019-07-23 21:21:04 +0700

    Update Sctv.vue

--- src/pages/Sctv.vue
+++ src/pages/Sctv.vue
@@ -1,1 +1,37 @@
-Rt
+<template>
+ <div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+            <iframe width="560" height="315" src="https://www.yantv.netlify.com/ply/Sctv.html" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       </div> <div class="list-group-item">Sctv Live 
+
+</div></div>
+
+  </template>
+
+<script>
+  import Yayan from 'src/components/Yayan/Yayan.vue'
+
+  export default {
+    components: {
+      Yayan
+    }
+  }
+
+</script>
+
+
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+<style>
+
+</style>
+
