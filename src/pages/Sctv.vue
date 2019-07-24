1370f96ed1f2b08af15e4e4c99eef1cb5dce8b84
yayanheeh <tokoxp58@gmail.com>
2019-07-24 09:58:45 +0700

    Update Sctv.vue

--- src/pages/Sctv.vue
+++ src/pages/Sctv.vue
@@ -2,7 +2,7 @@
  <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
-            <iframe width="560" height="315" src="http://yantv.netlify.com/ply/sctv" frameborder="0" allowfullscreen></iframe>    </div> <div class="list-group-item">Sctv Live 
+            <iframe width="560" height="315" src="https://yantv.netlify.com/ply/sctv" frameborder="0" allowfullscreen></iframe>    </div> <div class="list-group-item">Sctv Live 
 
 </div></div>
 
