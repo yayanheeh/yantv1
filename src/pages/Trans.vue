565b865e96406c700957003fefa47c2ea32be867
yayanheeh <tokoxp58@gmail.com>
2019-07-24 15:22:46 +0700

    Update Trans.vue

--- src/pages/Trans.vue
+++ src/pages/Trans.vue
@@ -1,26 +1,23 @@
+ 
 <template>
- <div class="sticky-top">
-<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://yantv.netlify.com/ply/trans' style='border:0'></iframe></div>
+<div class="sticky-top">
 
+<div class="embed-responsive embed-responsive-16by9">
+      <iframe src="https://tv.detik.com/transtv/embed?smartautoplay=true" frameborder="0" allowfullscreen="true" id="video_play" name="video_play"></iframe> </div>
+<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Trans tv live </div>
 
- <div class="list-group-item">Trans Tv Live 
-
-</div></div>
-
-  </template>
-
-
-
+</div>
+</template>
 <script>
   import Card from 'src/components/Cards/Card.vue'
-
   export default {
     components: {
       Card
     }
   }
-
 </script>
+             
 <style>
 
 </style>
