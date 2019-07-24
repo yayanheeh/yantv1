39b19779133748727a226bd3fc8e8bcd5364b4e4
yayanheeh <tokoxp58@gmail.com>
2019-07-24 10:20:34 +0700

    Update Sctv.vue

--- src/pages/Sctv.vue
+++ src/pages/Sctv.vue
@@ -4,7 +4,7 @@
 <div class="embed-responsive embed-responsive-16by9">
            <iframe src="https://yantv.netlify.com/ply/sctv" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>   
  </div> 
-<div class="list-group-item">Sctv Live </div>
+<div class="list-group-item"><img src="https://img.icons8.com/ios/24/000000/rfid-signal.png"> Sctv Live </div>
 
 </div>
 
