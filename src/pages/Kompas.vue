c568f1f60bec08bb46d55f9e2b841ddf34a9ec13
yayanheeh <tokoxp58@gmail.com>
2019-07-24 15:45:24 +0700

    Create Kompas.vue

---
+++ src/pages/Kompas.vue
@@ -0,0 +1,24 @@
+ 
+<template>
+<div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/T6b5TAey728" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div> <div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Kompas tv live </div>
+
+</div>
+</template>
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+  export default {
+    components: {
+      Card
+    }
+  }
+</script>
+             
+<style>
+
+</style>
+
