3a5c69080efc085fe6e5194e79fc486905661376
yayanheeh <tokoxp58@gmail.com>
2019-07-24 14:42:29 +0700

    Update Sctv.vue

--- src/pages/Sctv.vue
+++ src/pages/Sctv.vue
@@ -6,7 +6,8 @@
         <source src="http://202.80.222.178/000001/2/ch15010918464887721048/index.m3u8?virtualDomain=000001.live_hls.zte.com&IASHttpSessionId=OTT11209201907240349063715746p" type="video/mp4">
     </video>
 </div>
-<div class="list-group-item"><img src="https://img.icons8.com/ios/18/000000/rfid-signal.png"> Sctv Live </div>
+<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Sctv Live </div>
 
 </div>
 
@@ -34,6 +35,10 @@
   }
 
 </script>
+
+              
+
+
 <style>
 
 </style>
