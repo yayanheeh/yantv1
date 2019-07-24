735517aadf5681e5096826083b785b0ae814d237
yayanheeh <tokoxp58@gmail.com>
2019-07-24 15:11:08 +0700

    Update Tvone.vue

--- src/pages/Tvone.vue
+++ src/pages/Tvone.vue
@@ -1,9 +1,8 @@
  <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
-  <iframe src="https://www.youtube.com/embed/EL2ggA7Z-Y4" allowfullscreen></iframe>
-</div>
-</div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/EL2ggA7Z-Y4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       </div>
 <div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
    Tv one live </div>
 
