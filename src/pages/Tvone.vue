12d6e7909ed27252b5cd837e68ba6cc76afc8fbf
yayanheeh <tokoxp58@gmail.com>
2019-07-24 15:15:16 +0700

    Update Tvone.vue

--- src/pages/Tvone.vue
+++ src/pages/Tvone.vue
@@ -1,4 +1,6 @@
- <div class="sticky-top">
+ 
+<template>
+<div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/EL2ggA7Z-Y4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -7,7 +9,7 @@
    Tv one live </div>
 
 </div>
-
+</template>
 <script>
   import Card from 'src/components/Cards/Card.vue'
   export default {
