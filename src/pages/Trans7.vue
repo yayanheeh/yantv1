66cc963cf478ba035ba55f236369e28edf081079
yayanheeh <tokoxp58@gmail.com>
2019-07-24 15:33:26 +0700

    Create Trans7.vue

---
+++ src/pages/Trans7.vue
@@ -0,0 +1,24 @@
+ 
+<template>
+<div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+      <iframe src="https://tv.detik.com/trans7/embed?smartautoplay=true" frameborder="0" allowfullscreen="true" id="video_play" name="video_play"></iframe> </div>
+<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Trans7 live </div>
+
+</div>
+</template>
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+  export default {
+    components: {
+      Card
+    }
+  }
+</script>
+             
+<style>
+
+</style>
+
