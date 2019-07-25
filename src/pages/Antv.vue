23a6470081c1d5560db75c6bdc6daa66b9dad00a
yayanheeh <tokoxp58@gmail.com>
2019-07-25 22:12:50 +0700

    Update Antv.vue

--- src/pages/Antv.vue
+++ src/pages/Antv.vue
@@ -1,9 +1,6 @@
 <template>
 
-<div class="d-none d-md-block">
-<div class="embed-responsive embed-responsive-16by9">
-<iframe src="https://www.metube.id/embed/206?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
-</div></div>
+
 <div class="d-block d-md-none">
 
  <div class="sticky-top">
