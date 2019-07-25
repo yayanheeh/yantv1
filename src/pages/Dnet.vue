e7fe34cdfb68f30865d787e8eabd1df6efd7ec44
yayanheeh <tokoxp58@gmail.com>
2019-07-25 22:19:03 +0700

    Create Dnet.vue

---
+++ src/pages/Dnet.vue
@@ -0,0 +1,7 @@
+<div class="d-none d-md-block">
+
+
+<div class="embed-responsive embed-responsive-16by9">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xO51BDBhPWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
+</div></div>
