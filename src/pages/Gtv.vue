7c54d90f8a798293f35220b350f79a7df5ce6ff1
yayanheeh <tokoxp58@gmail.com>
2019-07-25 19:55:20 +0700

    Update Gtv.vue

--- src/pages/Gtv.vue
+++ src/pages/Gtv.vue
@@ -1,4 +1,8 @@
 <template>
+
+<div class="d-none d-md-block">
+
+
 <div class="embed-responsive embed-responsive-16by9">
 <iframe src="https://www.metube.id/embed/2?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
 </div></div>
