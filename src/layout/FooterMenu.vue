608f117ff84ea5f713c9047459b49419bb68b0df
yayanheeh <tokoxp58@gmail.com>
2019-07-25 14:03:32 +0700

    Update FooterMenu.vue

--- src/layout/FooterMenu.vue
+++ src/layout/FooterMenu.vue
@@ -111,7 +111,7 @@
 
         <div class="card-img-overlay text-white d-flex flex-column justify-content-center">
           <span class="time">Live</span>
-         <center><router-link :to="{path:'/admin'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center>
+         <center><router-link :to="{path:'/admin/antv'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center>
      </div>   
 </div>
     </div>
@@ -121,7 +121,7 @@
 
         <div class="card-img-overlay text-white d-flex flex-column justify-content-center">
           <span class="time">Live</span>
-         <center><router-link :to="{path:'/admin'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center> </div>
+         <center><router-link :to="{path:'/admin/mnc'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center> </div>
         </div>
         
 
