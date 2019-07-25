94bac1823d8bc23af3163da10de358b98e692e2e
yayanheeh <tokoxp58@gmail.com>
2019-07-25 22:25:45 +0700

    Update Tvone.vue

--- src/pages/Tvone.vue
+++ src/pages/Tvone.vue
@@ -1,11 +1,6 @@
  
 <template>
-<div class="d-block d-md-block">
 
-
-<div class="embed-responsive embed-responsive-16by9">
-<iframe src="https://www.metube.id/embed/219?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
-</div></div>
 <div class="d-block d-md-none">
 
 
