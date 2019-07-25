8c8a100566401ad44f2c672f6755c1a7cf3ec0c1
yayanheeh <tokoxp58@gmail.com>
2019-07-25 22:23:16 +0700

    Create Dtras.vue

---
+++ src/pages/Dtras.vue
@@ -0,0 +1,6 @@
+<div class="d-none d-md-block">
+<div class="embed-responsive embed-responsive-16by9">
+
+
+<iframe src="https://www.metube.id/embed/178?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
+</div></div>
