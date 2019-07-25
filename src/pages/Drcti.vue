b97992f99e45ee5ccfea1efab70571b79a0d4e4b
yayanheeh <tokoxp58@gmail.com>
2019-07-25 22:20:19 +0700

    Create Drcti.vue

---
+++ src/pages/Drcti.vue
@@ -0,0 +1,7 @@
+<div class="d-none d-md-block">
+<div class="embed-responsive embed-responsive-16by9">
+
+
+<iframe src="https://www.metube.id/embed/1?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
+</div>
+</div>
