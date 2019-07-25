5a14caf286dcc0af7b021110fc6ff09a2f410c8d
yayanheeh <tokoxp58@gmail.com>
2019-07-25 19:47:05 +0700

    Update Antv.vue

--- src/pages/Antv.vue
+++ src/pages/Antv.vue
@@ -2,7 +2,7 @@
 
 <div class="d-none d-md-block">
 <div class="embed-responsive embed-responsive-16by9">
-Conten desktop
+<iframe src="https://www.metube.id/embed/206?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
 </div></div>
 <div class="d-block d-md-none">
 
