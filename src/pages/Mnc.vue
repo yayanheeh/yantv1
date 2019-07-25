0fe9fc9f30147e90b318d22141e03caa5f0e65b9
yayanheeh <tokoxp58@gmail.com>
2019-07-25 14:07:03 +0700

    Create Mnc.vue

---
+++ src/pages/Mnc.vue
@@ -0,0 +1,44 @@
+<template>
+ <div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/mnc.html" allowfullscreen></iframe>
+</div>
+
+<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Mnc tv Live </div>
+
+</div>
+
+  </template>
+
+<script>
+  import Yayan from 'src/components/Yayan/Yayan.vue'
+
+  export default {
+    components: {
+      Yayan
+    }
+  }
+
+</script>
+
+
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+
+              
+
+
+<style>
+
+</style>
+
