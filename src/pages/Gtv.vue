c50f8cf9419d7cd832dfca8df19fa80bed11fcab
yayanheeh <tokoxp58@gmail.com>
2019-07-25 15:40:49 +0700

    Create Gtv.vue

---
+++ src/pages/Gtv.vue
@@ -0,0 +1,35 @@
+<template>
+ <div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/gtv.html" allowfullscreen></iframe>
+</div>
+
+<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Gtv Live </div>
+
+</div>
+
+  </template>
+
+
+
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+
+              
+
+
+<style>
+
+</style>
+
+
