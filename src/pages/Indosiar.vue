e33e853cc68e48da8a3ef8bc4bac6fbffc0eb257
yayanheeh <tokoxp58@gmail.com>
2019-07-25 19:39:20 +0700

    Update Indosiar.vue

--- src/pages/Indosiar.vue
+++ src/pages/Indosiar.vue
@@ -1,4 +1,10 @@
 <template>
+<div class="embed-responsive embed-responsive-16by9">
+Conten desktop
+</div></div>
+<div class="d-block d-md-none">
+
+
  <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
@@ -9,7 +15,7 @@
    Indosiar Live </div>
 
 </div>
-
+</div>
   </template>
 
 
