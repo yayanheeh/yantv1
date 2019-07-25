2b1ba77697aab7c7d34a347f5117fd489ba12a75
yayanheeh <tokoxp58@gmail.com>
2019-07-25 22:09:18 +0700

    Update Mnc.vue

--- src/pages/Mnc.vue
+++ src/pages/Mnc.vue
@@ -1,11 +1,5 @@
 <template>
 
-<div class="d-none d-md-block">
-
-
-<div class="embed-responsive embed-responsive-16by9">
-<iframe src="https://www.metube.id/embed/3?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
-</div></div>
 <div class="d-block d-md-none">
 
 
@@ -20,6 +14,14 @@
 
 </div>
 </div>
+
+<div class="d-none d-md-block">
+
+
+<div class="embed-responsive embed-responsive-16by9">
+<iframe src="https://www.metube.id/embed/3?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
+</div></div>
+
   </template>
 
 <script>
