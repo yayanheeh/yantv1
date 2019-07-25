1fb6cceccf99877b80058646c13382fdc81edba0
yayanheeh <tokoxp58@gmail.com>
2019-07-25 19:38:39 +0700

    Update Gtv.vue

--- src/pages/Gtv.vue
+++ src/pages/Gtv.vue
@@ -1,4 +1,10 @@
 <template>
+<div class="embed-responsive embed-responsive-16by9">
+Conten desktop
+</div></div>
+<div class="d-block d-md-none">
+
+
  <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
@@ -8,7 +14,7 @@
 <div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
    Gtv Live </div>
 
-</div>
+</div></div>
 
   </template>
 
