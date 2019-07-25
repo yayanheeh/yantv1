046d63e74ea2e78fee7f8b44953699c1dada68d8
yayanheeh <tokoxp58@gmail.com>
2019-07-25 15:01:35 +0700

    Update FooterMenu.vue

--- src/layout/FooterMenu.vue
+++ src/layout/FooterMenu.vue
@@ -130,6 +130,35 @@
     
     </div>
 
+ <hr/>
+Tv sport
+<div class="row">
+    <div class="col">
+    <div class="card">
+         <img src="https://statik.tempo.co/data/2011/11/18/id_97139/97139_620.jpg" class="img-thumbnail" alt="Responsive image">
+
+        <div class="card-img-overlay text-white d-flex flex-column justify-content-center">
+          <span class="time">Live</span>
+         <center><router-link :to="{path:'/admin/realmadrid'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center>
+     </div>   
+</div>
+    </div>
+    <div class="col">
+    <div class="card">
+          <img src="https://ik.imagekit.io/ulangotv/image/upload/tr:n-small_tn/250914_logo_eurosport_2.png" class="img-thumbnail" alt="Responsive image">
+
+        <div class="card-img-overlay text-white d-flex flex-column justify-content-center">
+          <span class="time">Live</span>
+         <center><router-link :to="{path:'/admin/euro'}"> <img src="https://img.icons8.com/color/48/000000/circled-play.png"width="30"></router-link></center> </div>
+        </div>
+        
+
+    </div>
+    
+    
+    </div>
+
+
     </div>
     
     
