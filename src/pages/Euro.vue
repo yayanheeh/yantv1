ab84ee29384ac0694dccbeefd512fc77cd6e5945
yayanheeh <tokoxp58@gmail.com>
2019-07-25 14:55:24 +0700

    Create Euro.vue

---
+++ src/pages/Euro.vue
@@ -0,0 +1,44 @@
+<template>
+ <div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/eurosport.html" allowfullscreen></iframe>
+</div>
+
+<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Eurosport Live </div>
+
+</div>
+
+  </template>
+
+<script>
+  import Yayan from 'src/components/Yayan/Yayan.vue'
+
+  export default {
+    components: {
+      Yayan
+    }
+  }
+
+</script>
+
+
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+
+              
+
+
+<style>
+
+</style>
+
