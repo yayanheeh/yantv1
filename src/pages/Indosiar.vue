e2170babfce958e2615f0fa5b5a97914ca180f1e
yayanheeh <tokoxp58@gmail.com>
2019-07-25 15:40:05 +0700

    Update Indosiar.vue

--- src/pages/Indosiar.vue
+++ src/pages/Indosiar.vue
@@ -1,1 +1,35 @@
-Gg
+<template>
+ <div class="sticky-top">
+
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/indosiar.html" allowfullscreen></iframe>
+</div>
+
+<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
+   Indosiar Live </div>
+
+</div>
+
+  </template>
+
+
+
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+
+              
+
+
+<style>
+
+</style>
+
+
