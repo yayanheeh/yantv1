dde37298d666ba2ed2660ceeab650bfd2c49da17
yayanheeh <tokoxp58@gmail.com>
2019-07-25 19:54:27 +0700

    Update Net.vue

--- src/pages/Net.vue
+++ src/pages/Net.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="embed-responsive embed-responsive-16by9">
-Conten desktop
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xO51BDBhPWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+
 </div></div>
 <div class="d-block d-md-none">
 
