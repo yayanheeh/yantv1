e4047c5006a5fcb734fe7191fad7ffdc23b61b74
yayanheeh <tokoxp58@gmail.com>
2019-07-25 16:49:53 +0700

    Update Net.vue

--- src/pages/Net.vue
+++ src/pages/Net.vue
@@ -2,8 +2,7 @@
  <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/net.html" allowfullscreen></iframe>
-</div>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/xO51BDBhPWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 
 <div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
    Net tv Live </div>
