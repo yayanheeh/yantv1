6ad5a04dec3e32898bc90b977d9bb399c23a4fd5
yayanheeh <tokoxp58@gmail.com>
2019-07-26 12:42:42 +0700

    Update Dantv.vue

--- src/pages/Dantv.vue
+++ src/pages/Dantv.vue
@@ -5,11 +5,12 @@
 
 
  <div class="sticky-top">
+<div class="p-3 mb-2 bg-dark text-white">
 
 <div class="embed-responsive embed-responsive-16by9">
   <iframe src="https://www.metube.id/embed/206?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen></div>
 </div>
-
+</div>
 <div class="d-block d-md-none">
 <div class="list-group-item">
  <div class="row">
