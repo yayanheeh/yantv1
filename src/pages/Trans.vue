23c14b9da511af20c58ef6688f8ee3a6afd133b4
yayanheeh <tokoxp58@gmail.com>
2019-07-26 14:28:31 +0700

    Update Trans.vue

--- src/pages/Trans.vue
+++ src/pages/Trans.vue
@@ -2,15 +2,25 @@
 <template>
 
 
-<div class="d-block d-md-none">
 <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
       <iframe src="https://tv.detik.com/transtv/embed?smartautoplay=true" frameborder="0" allowfullscreen="true" id="video_play" name="video_play"></iframe> </div>
-<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
-   Trans tv live </div>
+<div class="list-group-item">
+ <div class="row">
+    <div class="col">
+  <i class="nc-icon nc-chart-bar-32"></i>
+   Trans tv live  </div>   
+    
+    <div class="col">
+    
+        <router-link :to="{path:'/admin/Dtrans'}">&ensp;&ensp;&ensp; <small><i class="nc-icon nc-button-play"></i> Server 2</small> </router-link></div>
+        </div>
+        
 
-</div></div>
+    </div>
+ </div>
+</div>
 </template>
 <script>
   import Card from 'src/components/Cards/Card.vue'
