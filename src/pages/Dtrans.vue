5ad8e7222e9ceae84b1a00bc538cf2de36acdcfa
yayanheeh <tokoxp58@gmail.com>
2019-07-26 13:16:44 +0700

    Update Dtrans.vue

--- src/pages/Dtrans.vue
+++ src/pages/Dtrans.vue
@@ -1,23 +1,8 @@
 <template>
 
 <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/metro.html" allowfullscreen></iframe>
-</div>
+ <iframe src="https://www.metube.id/embed/12101841?type=video&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen></div>
 
-<div class="list-group-item">
- <div class="row">
-    <div class="col">
-  <i class="nc-icon nc-chart-bar-32"></i>
-   Metro tv Live  </div>   
-    
-    <div class="col">
-    
-        <router-link :to="{path:'/admin/sctv'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> Chanel 2 </router-link></div>
-        </div>
-        
-
-    </div>
- </div>
 
 
 
