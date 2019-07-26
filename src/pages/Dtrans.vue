e9af5463feccfee2f93472def76cccebfeda1de5
yayanheeh <tokoxp58@gmail.com>
2019-07-26 15:54:05 +0700

    Update Dtrans.vue

--- src/pages/Dtrans.vue
+++ src/pages/Dtrans.vue
@@ -3,7 +3,7 @@
 <div class="embed-responsive embed-responsive-16by9">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/DGqru--nBoQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-
+</div>
 
   </template>
 
