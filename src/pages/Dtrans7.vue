a95e17439863cc4cc176ed4da23114ef23dd4885
yayanheeh <tokoxp58@gmail.com>
2019-07-26 22:12:44 +0700

    Update Dtrans7.vue

--- src/pages/Dtrans7.vue
+++ src/pages/Dtrans7.vue
@@ -3,17 +3,13 @@
 <div class="embed-responsive embed-responsive-16by9">
 <iframe src="https://www.metube.id/embed/183?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen></div>
 
-
-
-
-
   </template>
 
-
-<link rel="alternate" media="only screen and (max-width: 640px)"
- href="https://yantv.netlify.com/#/admin/kompas"/>
-
-
+<script type="text/javascript">
+        if (screen.width <= 720) {
+        window.location = "https://yantv.netlify.com/#/admin/kompas";
+    }
+</script>
 <script>
   import Card from 'src/components/Cards/Card.vue'
 
