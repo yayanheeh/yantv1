72d8641483174de920ac892a716b9664b5479472
yayanheeh <tokoxp58@gmail.com>
2019-07-26 21:57:11 +0700

    Update Tentangkami.vue

--- src/pages/Tentangkami.vue
+++ src/pages/Tentangkami.vue
@@ -1,1 +1,7 @@
-Trszzz
+<template>
+Tentang Kami
+Yplay TV adalah situs web yang memiliki tayangan video/Tv Online Yang terkonsep, selalu diperbaharui terus-menerus, mengikuti perkembangan peristiwa yang terjadi di lingkungan sekitar, dan bisa diakses oleh publik secara bebas, dengan berbagai macam bentuk pendistribusiannya. Untuk dapat mengaksesnya, kita hanya perlu menguhubungkan ke komputer atau handpine pribadi kita yang pasti dengan koneksi Internet
+Ide Dasar
+Yplay TV memudahkan pengguna Internet di Indonesia yang ingin mengakses situs televisi Online dari dalam maupun luar negeri untuk mendapatkan informasi atau sekadar menonton acara televisi luar tanpa harus menggunakan televisi berlangganan yang perlu dibayar setiap bulannya,
+
+</template>
