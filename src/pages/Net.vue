3475cce534cc20475d6130145bc8cf546137cbbc
yayanheeh <tokoxp58@gmail.com>
2019-07-26 14:24:52 +0700

    Update Net.vue

--- src/pages/Net.vue
+++ src/pages/Net.vue
@@ -1,7 +1,6 @@
 <template>
 
 
-<div class="d-block d-md-none">
 
 
  <div class="sticky-top">
@@ -9,10 +8,21 @@
 <div class="embed-responsive embed-responsive-16by9">
   <iframe width="560" height="315" src="https://www.youtube.com/embed/xO51BDBhPWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 
-<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
-   Net tv Live </div>
+<div class="list-group-item">
+ <div class="row">
+    <div class="col">
+  <i class="nc-icon nc-chart-bar-32"></i>
+   Net tv live  </div>   
+    
+    <div class="col">
+    
+        <router-link :to="{path:'/admin/Dnet'}">&ensp;&ensp;&ensp; <small><i class="nc-icon nc-button-play"></i> Server 2</small> </router-link></div>
+        </div>
+        
 
-</div></div>
+    </div>
+ </div>
+</div>
 
   </template>
 
