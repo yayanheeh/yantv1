5a94813b0bd225363b0aa3d6af637e974a00d890
yayanheeh <tokoxp58@gmail.com>
2019-07-26 11:58:00 +0700

    Create Dantv.vue

---
+++ src/pages/Dantv.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/metro.html" allowfullscreen></iframe>
+</div>
+
+<div class="list-group-item">
+ <div class="row">
+    <div class="col">
+  <i class="nc-icon nc-chart-bar-32"></i>
+   Metro tv Live  </div>   
+    
+    <div class="col">
+    
+        <router-link :to="{path:'/admin/sctv'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> Chanel 2 </router-link></div>
+        </div>
+        
+
+    </div>
+ </div>
+
+
+
+  </template>
+
+
+
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+
+              
+
+
+<style>
+
+</style>
+
