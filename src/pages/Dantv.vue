b6b8f2f3f674b068bc5e6f14ea529e8e21e59575
yayanheeh <tokoxp58@gmail.com>
2019-07-26 12:57:24 +0700

    Update Dantv.vue

--- src/pages/Dantv.vue
+++ src/pages/Dantv.vue
@@ -10,24 +10,20 @@
 <div class="embed-responsive embed-responsive-16by9">
   <iframe src="https://www.metube.id/embed/206?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen></div>
 </div>
-</div>
-<div class="d-block d-md-none">
-<div class="list-group-item">
- <div class="row">
+<div class="row">
     <div class="col">
   <i class="nc-icon nc-chart-bar-32"></i>
-   Metro tv Live  </div>   
+   Antv Live  </div>   
     
     <div class="col">
     
-        <router-link :to="{path:'/admin/sctv'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> Chanel 2 </router-link></div>
+        <router-link :to="{path:'/admin/antv'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> Server 1 </router-link></div>
         </div>
-        
 
-    </div>
- </div>
+
 </div>
 </div>
+
 
   </template>
 
