fb9b4ba96aa3377529f36ccd0c36335d24b1d9a1
yayanheeh <tokoxp58@gmail.com>
2019-07-26 15:41:50 +0700

    Update DashboardLayout.vue

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -14,25 +14,25 @@
         <i class="nc-icon nc-circle-09"></i>
         <p>User Profile</p>
       </sidebar-link>
-      <sidebar-link to="/admin/table-list">
-        <i class="nc-icon nc-notes"></i>
-        <p>Table list</p>
+      <sidebar-link to="/admin/dtrans">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Trans tv</p>
       </sidebar-link>
-      <sidebar-link to="/admin/typography">
-        <i class="nc-icon nc-paper-2"></i>
-        <p>Typography</p>
+      <sidebar-link to="/admin/dtrans7">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Trans7</p>
       </sidebar-link>
-      <sidebar-link to="/admin/icons">
-        <i class="nc-icon nc-atom"></i>
-        <p>Icons</p>
+      <sidebar-link to="/admin/dkompas">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Kompas tv</p>
       </sidebar-link>
-      <sidebar-link to="/admin/maps">
-        <i class="nc-icon nc-pin-3"></i>
-        <p>Maps</p>
+      <sidebar-link to="/admin/dmetro">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Metro tv</p>
       </sidebar-link>
-      <sidebar-link to="/admin/notifications">
-        <i class="nc-icon nc-bell-55"></i>
-        <p>Notifications</p>
+      <sidebar-link to="/admin/dsctv">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Sctv</p>
       </sidebar-link>
 
       <template slot="bottom-links">
