7278d3b1d43300cde88e122461109772b6a8e9dc
yayanheeh <tokoxp58@gmail.com>
2019-07-26 14:29:57 +0700

    Update Trans7.vue

--- src/pages/Trans7.vue
+++ src/pages/Trans7.vue
@@ -1,21 +1,25 @@
  
 <template>
-<div class="d-none d-md-block">
-<div class="embed-responsive embed-responsive-16by9">
-
-
-<iframe src="https://www.metube.id/embed/183?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen>
-</div></div>
-<div class="d-block d-md-none">
 
 <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
       <iframe src="https://tv.detik.com/trans7/embed?smartautoplay=true" frameborder="0" allowfullscreen="true" id="video_play" name="video_play"></iframe> </div>
-<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
-   Trans7 live </div>
+<div class="list-group-item">
+ <div class="row">
+    <div class="col">
+  <i class="nc-icon nc-chart-bar-32"></i>
+   Trans7 Live  </div>   
+    
+    <div class="col">
+    
+        <router-link :to="{path:'/admin/Dtrans7'}">&ensp;&ensp;&ensp; <small><i class="nc-icon nc-button-play"></i> Server 2</small> </router-link></div>
+        </div>
+        
 
-</div></div>
+    </div>
+ </div>
+</div>
 </template>
 <script>
   import Card from 'src/components/Cards/Card.vue'
