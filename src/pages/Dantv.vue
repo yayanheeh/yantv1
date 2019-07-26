37721affcf9bf75ba6910ddc69945bbd93ebe0bf
yayanheeh <tokoxp58@gmail.com>
2019-07-26 13:03:39 +0700

    Update Dantv.vue

--- src/pages/Dantv.vue
+++ src/pages/Dantv.vue
@@ -4,26 +4,10 @@
 
 
 
- <div class="sticky-top">
-<div class="p-3 mb-2 bg-dark text-white">
-
 <div class="embed-responsive embed-responsive-16by9">
   <iframe src="https://www.metube.id/embed/206?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen></div>
 </div>
-<div class="row">
-    <div class="col">
-  <i class="nc-icon nc-chart-bar-32"></i>
-   Antv Live  </div>   
-    
-    <div class="col">
-    
-        <router-link :to="{path:'/admin/antv'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> Server 1 </router-link></div>
-        </div>
-
-
-</div>
-</div>
-
+<br/>
 
   </template>
 
