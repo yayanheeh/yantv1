0d508e4e3ca1712ad788c03c4d7f60bfc1913a66
yayanheeh <tokoxp58@gmail.com>
2019-07-26 12:33:54 +0700

    Update Dantv.vue

--- src/pages/Dantv.vue
+++ src/pages/Dantv.vue
@@ -1,29 +1,45 @@
 <template>
 
+
+
+
+
+ <div class="sticky-top">
+
 <div class="embed-responsive embed-responsive-16by9">
   <iframe src="https://www.metube.id/embed/206?type=live&autoplay=true&mute=true" width="560" height="315" frameborder="0" allowfullscreen></div>
+</div>
 
 <div class="d-block d-md-none">
-
 <div class="list-group-item">
  <div class="row">
     <div class="col">
   <i class="nc-icon nc-chart-bar-32"></i>
-   Antv Live  </div>   
+   Metro tv Live  </div>   
     
     <div class="col">
     
-        <router-link :to="{path:'/admin/antv'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> Server 1 </router-link></div>
+        <router-link :to="{path:'/admin/sctv'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> Chanel 2 </router-link></div>
         </div>
         
 
     </div>
  </div>
-
-
+</div>
+</div>
 
   </template>
 
+<script>
+  import Yayan from 'src/components/Yayan/Yayan.vue'
+
+  export default {
+    components: {
+      Yayan
+    }
+  }
+
+</script>
 
 
 <script>
@@ -43,4 +59,9 @@
 <style>
 
 </style>
+
+
+
+
+
 
