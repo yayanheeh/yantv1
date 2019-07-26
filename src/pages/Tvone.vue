94c0f3bb15588e351b8cf760300c91fcf9769c20
yayanheeh <tokoxp58@gmail.com>
2019-07-26 14:30:55 +0700

    Update Tvone.vue

--- src/pages/Tvone.vue
+++ src/pages/Tvone.vue
@@ -1,7 +1,6 @@
  
 <template>
 
-<div class="d-block d-md-none">
 
 
 <div class="sticky-top">
@@ -9,10 +8,21 @@
 <div class="embed-responsive embed-responsive-16by9">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/EL2ggA7Z-Y4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
        </div>
-<div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
-   Tv one live </div>
+<div class="list-group-item">
+ <div class="row">
+    <div class="col">
+  <i class="nc-icon nc-chart-bar-32"></i>
+   Tvone Live  </div>   
+    
+    <div class="col">
+    
+        <router-link :to="{path:'/admin/Dtvone'}">&ensp;&ensp;&ensp; <small><i class="nc-icon nc-button-play"></i> Server 2</small> </router-link></div>
+        </div>
+        
 
-</div></div>
+    </div>
+ </div>
+</div>
 </template>
 <script>
   import Card from 'src/components/Cards/Card.vue'
