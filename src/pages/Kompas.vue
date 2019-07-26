76dfdc32b6a271e0bfcef6e7ff8dceaefb243c47
yayanheeh <tokoxp58@gmail.com>
2019-07-26 10:44:50 +0700

    Update Kompas.vue

--- src/pages/Kompas.vue
+++ src/pages/Kompas.vue
@@ -9,8 +9,8 @@
 
 <div class="embed-responsive embed-responsive-16by9">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/T6b5TAey728" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div> <div class="list-group-item"><i class="nc-icon nc-chart-bar-32"></i>
-   Kompas tv live </div>
+</div> <div class="list-group-item"><i class="nc-icon nc-bell-55"></i>
+   Kompas tv live <i class="nc-icon nc-bell-55"></i> </div>
 
 </div></div>
 </template>
