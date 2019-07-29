f626a71499a0eb1a6c0cb56580fc3d013984af04
yayanheeh <tokoxp58@gmail.com>
2019-07-29 11:03:57 +0700

    Update Kritik.vue

--- src/pages/Kritik.vue
+++ src/pages/Kritik.vue
@@ -1,3 +1,4 @@
 <template>
-Tes
+<iframe src="https://www4.cbox.ws/box/?boxid=4337386&boxtag=rTsCNa" width="100%" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>	
+
 </template>
