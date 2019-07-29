5c60f0a6546ed3144cd2549f2bcd1df18aa04037
yayanheeh <tokoxp58@gmail.com>
2019-07-29 09:59:34 +0700

    Update Net.vue

--- src/pages/Net.vue
+++ src/pages/Net.vue
@@ -1,27 +1,26 @@
 <template>
 
-
-
-
  <div class="sticky-top">
 
 <div class="embed-responsive embed-responsive-16by9">
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/xO51BDBhPWw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  <iframe class="embed-responsive-item" src="https://yantv.netlify.com/play/net.html" allowfullscreen></iframe>
+</div>
 
 <div class="list-group-item">
  <div class="row">
     <div class="col">
   <i class="nc-icon nc-chart-bar-32"></i>
-   Net tv live  </div>   
+   Netv Live  </div>   
     
     <div class="col">
     
-        <router-link :to="{path:'/admin/Dnet'}">&ensp;&ensp;&ensp; <small><i class="nc-icon nc-button-play"></i> Server 2</small> </router-link></div>
+        <router-link :to="{path:'/admin/Dnet'}">&ensp;&ensp; <i class="nc-icon nc-button-play"></i> <small>Server 2</small> </router-link></div>
         </div>
         
 
     </div>
  </div>
+
 </div>
 
   </template>
@@ -49,10 +48,9 @@
 
 </script>
 
-              
-
-
+             
 <style>
 
 </style>
+
 
