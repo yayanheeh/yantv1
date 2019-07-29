28105a7b9baab776bde15ba23cfb7574e5496eba
yayanheeh <tokoxp58@gmail.com>
2019-07-29 19:44:50 +0700

    Update MobileMenu.vue

--- src/layout/MobileMenu.vue
+++ src/layout/MobileMenu.vue
@@ -4,19 +4,19 @@
 
   <ul class="nav nav-mobile-menu">
     <li class="nav-item">
-     <a class="nav-link" href="#tentangkami">
+     <a class="nav-link" href="https://tv.yplay.online/#admin/tentangkami">
         <span class="no-icon">Tentang kami</span>
       </a>
 </li>
 
  <li class="nav-item">
-   <a class="nav-link" href="#kritik">
+   <a class="nav-link" href="https://tv.yplay.online/#/admin/kritik">
         <span class="no-icon">Kritik/saran</span>
       </a>
     </li>
  <li class="nav-item">
   <a class="nav-link" href="https://instgram.com/kres_008">
-        <span class="nc-icon nc-camera-20">Kres_008</span>
+        <span class="no-icon nc-camera-20">Kres_008</span>
       </a>
     </li>
 
