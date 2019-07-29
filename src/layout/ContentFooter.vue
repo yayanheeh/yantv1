4c5cee97bdfb0733c203364a61350eb458b25762
yayanheeh <tokoxp58@gmail.com>
2019-07-29 10:42:17 +0700

    Update ContentFooter.vue

--- src/layout/ContentFooter.vue
+++ src/layout/ContentFooter.vue
@@ -2,18 +2,12 @@
 
   <footer class="footer">
       <div class="container-fluid">
-      <nav>
-        <ul class="footer-menu">
-          <li>
-            <router-link :to="{path:'/admin'}">Dashboard</router-link>
-          </li>
-        </ul>
-      </nav>
+     
       <div class="copyright text-center">
-        &copy; © 2019 -
+        &copy; 2019 -
         <i class="fa fa-heart heart"></i> by
-        <a href="https://yayanheeh.online" target="_blank">Yplay Tv</a>.
-        Designed by <a href="https://www.instagram.com/kres-008" target="_blank">Kres 008</a>.
+        <a href="https://yayanheeh.online" target="_blank">Yplay Tv</a>
+        Designed by <a href="https://www.instagram.com/kres_008" target="_blank">Kres 008</a>.
       </div>
     </div>
   </footer>
