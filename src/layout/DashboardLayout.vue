a1a116d4258ce31de7bbdc56a9ceedeebb7d7514
yayanheeh <tokoxp58@gmail.com>
2019-07-29 10:31:58 +0700

    Update DashboardLayout.vue

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -6,40 +6,52 @@
 <div class="d-none d-md-block">
     <side-bar>
       <mobile-menu slot="content"></mobile-menu>
-      <sidebar-link to="/admin/overview">
-        <i class="nc-icon nc-chart-pie-35"></i>
-        <p>Dashboard</p>
-      </sidebar-link>
-      <sidebar-link to="/admin/user">
-        <i class="nc-icon nc-circle-09"></i>
-        <p>User Profile</p>
-      </sidebar-link>
-      <sidebar-link to="/admin/dtrans">
-        <i class="nc-icon nc-chart-bar-32"></i>
-        <p>Trans tv</p>
+      <sidebar-link to="/admin/drcti">
+        <i class="nc-icon nc-chart-bar-35"></i>
+        <p>Rcti</p>
       </sidebar-link>
       <sidebar-link to="/admin/dtrans7">
-        <i class="nc-icon nc-chart-bar-32"></i>
+        <i class="nc-icon nc-circle-09"></i>
         <p>Trans7</p>
-      </sidebar-link>
-      <sidebar-link to="/admin/dkompas">
-        <i class="nc-icon nc-chart-bar-32"></i>
-        <p>Kompas tv</p>
       </sidebar-link>
       <sidebar-link to="/admin/dmetro">
         <i class="nc-icon nc-chart-bar-32"></i>
         <p>Metro tv</p>
       </sidebar-link>
-      <sidebar-link to="/admin/dsctv">
+      <sidebar-link to="/admin/dtvone">
         <i class="nc-icon nc-chart-bar-32"></i>
-        <p>Sctv</p>
+        <p>Tv one</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/dtrans">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Trans tv</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/dkompas">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Kompas tv</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/dnet">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Net tv</p>
+      </sidebar-link>
+
+  </sidebar-link>
+      <sidebar-link to="/admin/dantv">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Antv</p>
+      </sidebar-link>
+
+  </sidebar-link>
+      <sidebar-link to="/admin/dmnc">
+        <i class="nc-icon nc-chart-bar-32"></i>
+        <p>Mnc tv</p>
       </sidebar-link>
 
       <template slot="bottom-links">
         <sidebar-link class="active"
                       to="/admin/upgrade">
           <i class="nc-icon nc-alien-33"></i>
-          <p>Tes yayan</p>
+          <p>versi 1.0.1</p>
         </sidebar-link>
       </template>
     </side-bar>
